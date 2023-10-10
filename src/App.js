01a1dd8d47a65dc769e2b314035c812a9a90764a
juliawiktoria <julia.w.zieba@gmail.com>
2023-10-10 18:16:26 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import MainPage from './components/MainPage';
+import About from './components/About'; // Import the new AboutPage component
+import Projects from './components/Projects'; // Import the new ProjectsPage component
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <React.Suspense fallback={<div>Loading...</div>}>
-        <MainPage />
-      </React.Suspense>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/" element={<MainPage />} />
+          </Routes>
+        </React.Suspense>
+      </div>
+    </Router>
   );
 }
 
